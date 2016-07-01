d58cce6314a060f94b6d2486d862f42d84f2b127
Nicholas Ko <mailnickko@gmail.com>
2016-06-30 20:31:59 -0700

    basic reqs

---
+++ exercises/bare_minimum/basicChaining.js
@@ -0,0 +1,51 @@
+/*
+ * Write a function WITH NO CALLBACKS that,
+ * (1) reads a GitHub username from a `readFilePath`
+ *     (the username will be the first line of the file)
+ * (2) then, sends a request to the GitHub API for the user's profile
+ * (3) then, writes the JSON response of the API to `writeFilePath`
+ *
+ * HINT: We exported some similar promise-returning functions in previous exercises
+ */
+
+var fs = require('fs');
+var Promise = require('bluebird');
+var getGitHubProfileAsync = require('./promisification.js').getGitHubProfileAsync;
+var pluckFirst = require('./promiseConstructor.js').pluckFirstLineFromFileAsync;
+var asynWrite = Promise.promisify(fs.writeFile);
+
+
+var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
+  return pluckFirst(readFilePath)
+    .then(function(username) {
+      return getGitHubProfileAsync(username);
+    })
+    .then(function(userProfile) {
+      return asynWrite(writeFilePath, JSON.stringify(userProfile));
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+};
+
+// Export these functions so we can test them
+module.exports = {
+  fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
+};
+
+
+// danthareja
+
+// and
+// some
+// other
+
+// lines
+// we..
+
+// really
+// ...
+
+// don't
+// care
+// about
---
+++ exercises/bare_minimum/callbackReview.js
@@ -0,0 +1,34 @@
+/**
+ * Implement these functions following the node style callback pattern
+ */
+
+var fs = require('fs');
+var request = require('request');
+
+// This function should retrieve the first line of the file at `filePath`
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data.split('\n')[0]);
+    }
+  })
+};
+
+// This function should retrieve the status code of a GET request to `url`
+var getStatusCode = function (filePath, callback) {
+  request(filePath, function(err, res) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, res.statusCode);
+    }
+  })
+};
+
+// Export these functions so we can test them and reuse them in later exercises
+module.exports = {
+  getStatusCode: getStatusCode,
+  pluckFirstLineFromFile: pluckFirstLineFromFile
+};
